25da3dae582ed98c02a4c4afdff0c122a338a180
mateus <mateuszebendovp30@gmai.com>
2023-08-19 14:04:42 -0300

    Cria inputs na main e corrigi as variaveis das funcoes

---
+++ l3e10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void adicao (int nx, int dx, int ny, int dy, int r);
+
+void subtracao (int nx, int dx, int ny, int dy, int r);
+
+void multiplicacao (int nx, int dx, int ny, int dy, int r);
+
+void divisao (int nx, int dx, int ny, int dy, int r);
+
+void main (){
+
+    int nx, dx, ny, dy, r;
+
+    printf("Digite o numerador de X: ");
+    scanf("%d", &nx);
+    printf("Digite o denominador de X: ");
+    scanf("%d", &dx);
+    printf("Digite o numerador de Y: ");
+    scanf("%d", &ny);
+    printf("Digite o denominador de Y: ");
+    scanf("%d", &dy);
+}
