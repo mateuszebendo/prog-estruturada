a52d225b125a2ace3146d345153f3c1c7fce9401
mateus <mateuszebendovp30@gmai.com>
2023-08-19 01:07:28 -0300

    Funcao distancia efetiva o calculo

--- l2e6.c
+++ l2e6.c
@@ -9,13 +9,13 @@ void main (){
     int xa, xb, ya, yb;
     int r;
     printf("Digite o valor de Xa: ");
-    scanf("%d", xa);
+    scanf("%d", &xa);
     printf("Digite o valor de Xb: ");
-    scanf("%d", xb);
+    scanf("%d", &xb);
     printf("Digite o valor de Ya: ");
-    scanf("%d", ya);
+    scanf("%d", &ya);
     printf("Digite o valor de Yb: ");
-    scanf("%d", yb);
+    scanf("%d", &yb);
     r = distancia(xa,xb, ya, yb);
     printf("A distância entre esses dois pontos é: %d", r);
     getchar();
@@ -23,5 +23,12 @@ void main (){
 }
 
 int distancia (int xa, int xb, int ya, int yb){
+
+    int r1, r2, r3, r4;
+    r1 = pow(xa-xb, 2);
+    r2 = pow(ya-yb, 2);
+    r3 = r1 + r2;
+    r4 = sqrt(r3);
+
 
 }
