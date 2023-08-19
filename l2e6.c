553636cb06c6b2749269b085055352e90c161428
mateus <mateuszebendovp30@gmai.com>
2023-08-19 01:02:35 -0300

    Preenche a funcao main

--- l2e6.c
+++ l2e6.c
@@ -6,10 +6,22 @@ int distancia (int, int, int, int);
 
 void main (){
 
-
+    int xa, xb, ya, yb;
+    int r;
+    printf("Digite o valor de Xa: ");
+    scanf("%d", xa);
+    printf("Digite o valor de Xb: ");
+    scanf("%d", xb);
+    printf("Digite o valor de Ya: ");
+    scanf("%d", ya);
+    printf("Digite o valor de Yb: ");
+    scanf("%d", yb);
+    r = distancia(xa,xb, ya, yb);
+    printf("A distância entre esses dois pontos é: %d", r);
+    getchar();
 
 }
 
-int distancia (int xa, int xb, ya, yb){
+int distancia (int xa, int xb, int ya, int yb){
 
 }
