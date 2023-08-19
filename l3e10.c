ee221ab6758e6426698243d8c5652f277cb2d5e3
mateus <mateuszebendovp30@gmai.com>
2023-08-19 14:15:11 -0300

    Cria a funcao adicao e adiciona ponteiros

--- l3e10.c
+++ l3e10.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void adicao (int nx, int dx, int ny, int dy, int r);
+void adicao (int nx, int dx, int ny, int dy, int *nv, int *dv);
 
-void subtracao (int nx, int dx, int ny, int dy, int r);
+void subtracao (int nx, int dx, int ny, int dy, int *nv, int *dv);
 
-void multiplicacao (int nx, int dx, int ny, int dy, int r);
+void multiplicacao (int nx, int dx, int ny, int dy, int *nv, int *dv);
 
-void divisao (int nx, int dx, int ny, int dy, int r);
+void divisao (int nx, int dx, int ny, int dy, int *nv, int *dv);
 
 void main (){
 
-    int nx, dx, ny, dy, r;
+    int nx, dx, ny, dy, *nv, *dv;
 
     printf("Digite o numerador de X: ");
     scanf("%d", &nx);
@@ -21,4 +21,14 @@ void main (){
     scanf("%d", &ny);
     printf("Digite o denominador de Y: ");
     scanf("%d", &dy);
+    adicao(nx,dx,ny,dy,&nv,&dv);
+    printf("%d/%d", nv,dv);
+    getchar();
+}
+
+void adicao (int nx, int dx, int ny, int dy, int *nv, int *dv){
+
+    *nv= (nx*dy+ny*dx);
+    *dv = (dx*dy);
+
 }
